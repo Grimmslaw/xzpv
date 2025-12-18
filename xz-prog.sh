0011d6f96db87d657f63ffe5965eb5915fb827a0
dwrickey <d.wrickey@gmail.com>
2025-12-17 22:00:34 -0600

    script mostly finished, started man page

--- xz-prog.sh
+++ xz-prog.sh
@@ -3,20 +3,29 @@
 # TODO: add back when you actually understand it
 #set -euo pipefail
 
-getopt -T >/dev/null 2>&1
-
-if [ $? -ne 4 ]; then
-    echo "To run this script, getopt(1) must be installed and before getopt(3) on the path"
-    exit 1
-fi
+#------------------------
+# Variable Definitions
+#------------------------
 
 programname="$(basename "$0")"
 
 LOG_LEVEL=1
+COMP_LEVEL=0
 DEST=""
 DELETE=0
-FORCE_DELETE=0
 TARGET=""
+
+#------------------------
+# Function Definitions
+#------------------------
+
+check_getopt() {
+    getopt -T >/dev/null 2>&1
+    if [ $? -ne 4 ]; then
+        echo "To run this script, getopt(1) must be installed and before getopt(3) on the path"
+        exit 1
+    fi
+}
 
 exit_usage() {
     local status=1
@@ -36,12 +45,28 @@ usage() {
 Usage: $programname [options] <target>
 
 Options:
-  -d <dest>, --dest <dest>  Path (or name relative to cwd) of the archive file.
-  -v, --verbose             Verbose output.
-  -x, --delete              Delete the target folder after archiving.
-  -f, --delete-force        Delete the target folder after archiving (using the -f flag for the 'rm' command)
-  -l <level>, --log <level> Threshold for messages to be printed (0..4, default=1 [ERROR])
+  -c <level>, --comp <level>    Level of compression to be used with the xz command.
+  -d <dest>, --dest <dest>      Path (or name relative to cwd) of the archive file.
+  -l <level>, --log <level>     Threshold for output messages to be printed (0..4, default=1 [ERROR])
+  -s, --silent                  Silence output messages. (Equivalent of '-l 0'.)
+  -v, --verbose                 Verbose output. (Equivalent of '-l 4'.)
+  -x, --delete                  Delete the target folder after archiving.
 EOF
+}
+
+archive() {
+    local -r compression_level=$([ "$COMP_LEVEL" -gt 0 ] && echo "-$COMP_LEVEL" || echo "")
+    tar cf - "$TARGET" | pv -s $(($(du -sk "$TARGET" | awk '{print $1}') * 1024)) | xz "$compression_level" > "$ARCHIVE"
+    if [ ! -f "$ARCHIVE" ]; then
+        log ERROR "Failed to create archive $ARCHIVE"
+        exit 1
+    fi
+}
+
+cleanup() {
+    if [[ $DELETE -eq 1 ]]; then
+        rm -rf "$TARGET"
+    fi
 }
 
 interpret_log_level() {
@@ -72,49 +97,60 @@ log() {
     local -r log_at=$(interpret_log_level "$1")
     local -r message="$2"
     if [[ ( ( "$LOG_LEVEL" -ne 0 ) && ( ( "$log_at" = "ALL" ) || ( "$log_at" -ge "$LOG_LEVEL" ) ) ) ]]; then
-        printf "%s\n" $message
+        printf "%s\n" "$message"
     fi
 }
 
-INITIAL_PARSED=$(getopt -o "hd:xfl:v" -l "help,dest:,delete,delete-force,log:,verbose,debug" -n "$programname" -- "$@")
+#------------------------
+# Option Parsing
+#------------------------
+
+SHORT_OPTS="hc:d:l:svx"
+LONG_OPTS="help,comp:,dest:,log:,silent,verbose"
+INITIAL_PARSED=$(getopt -o "$SHORT_OPTS" -l "$LONG_OPTS" -n "$programname" -- "$@")
 if [ $? -ne 0 ]; then
     log ALL "Error parsing options." >&2
     exit_usage 1
 fi
+
 eval set -- "$INITIAL_PARSED"
+
 while true; do
     case "$1" in
-        --debug)
-            LOG_LEVEL=4; shift ;;
         -h|--help)
             exit_usage 0 ;;
-        -l|--log)
+        -c|--level)
             if [ -z "$2" ]; then
-                log ERROR "Error: Option -$1 requires an argument." >&2
+                log ERROR "Error: Option -$1 requires an argument."
+                exit_usage 1
+            elif [[ ("$2" -lt 1) || ("$2" -gt 9) ]]; then
+                log ERROR "Error: Argument for option -$1 must be between 1 and 9, inclusive."
                 exit_usage 1
             fi
             LOG_LEVEL=$(interpret_log_level "$2"); shift; shift ;;
         -d|--dest)
             if [ -z "$2" ]; then
-                log ERROR "Error: Option -$1 requires an argument." >&2
+                log ERROR "Error: Option -$1 requires an argument."
                 exit_usage 1
             fi
-            DEST="$2"; shift; shift  ;;
+            DEST="$2"; shift; shift ;;
+        -l|--log)
+            if [ -z "$2" ]; then
+                log ERROR "Error: Option -$1 requires an argument."
+                exit_usage 1
+            fi
+            LOG_LEVEL="$2"; shift; shift ;;
+        -s|--silent)
+            LOG_LEVEL=0; shift ;;
         -v|--verbose)
             LOG_LEVEL=3; shift ;;
-        -f|--delete-force)
-            DELETE=1; FORCE_DELETE=1; shift ;;
         -x|--delete)
             DELETE=1; shift ;;
-        --)
-            shift; break ;;
         *)
-            log ERROR "Internal error in option processing ($1)" >&2
+            log ERROR "Internal error in option processing ($1)"
             exit_usage 1 ;;
     esac
 done
-
-log DEBUG "programname=$programname, DEST=$DEST, VERBOSE=$VERBOSE, DELETE=$DELETE, @=$*"
 
 if [[ "$#" -lt 1 ]]; then
     log ERROR "A <target> positional argument is required."
@@ -126,15 +162,10 @@ fi
 TARGET="${1}"
 ARCHIVE="$DEST".tar.xz
 
-tar cf - "$TARGET" | pv -s $(($(du -sk "$TARGET" | awk '{print $1}') * 1024)) | xz > "$ARCHIVE"
-if [ ! -f "$ARCHIVE" ]; then
-    log ERROR "Failed to create archive $ARCHIVE"
-    exit 1
-fi
+#------------------------
+# Program Flow
+#------------------------
 
-if [[ $FORCE_DELETE -eq 1 ]]; then
-    rm -rf "$TARGET"
-elif [[ $DELETE -eq 1 ]]; then
-    rm -r "$TARGET"
-fi
-
+check_getopt
+archive
+cleanup
